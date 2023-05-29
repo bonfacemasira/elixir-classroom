44ef806bd97409f94e29d541dd9587689fa6b50f
Bonface Masira <bonfacemasira@gmail.com>
2023-05-29 23:29:26 +0300

    progress

---
+++ elixir/basics.ex
@@ -0,0 +1,12 @@
+# IO.puts "Hello world"
+life = 42
+
+name = "Bonface"
+IO.puts(life)
+IO.puts(name)
+
+# def hello do
+#   name = "Bonface"
+#   # IO.puts "Hello #{name}!"
+#   IO.puts("Hello " <> name)
+# end
